be2d44d18d479dee9bbc595da4c656711f56f145
PratikGarai <pratikgarai0208@gmail.com>
2020-12-16 02:42:28 +0530

    Accounts templates added and pipelined

--- socialProject/accounts/forms.py
+++ socialProject/accounts/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     class Meta :
-        fields = ('usrname', 'email', 'password1', 'password2')
         model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- socialProject/socialProject/views.py
+++ socialProject/socialProject/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "Index.html", {"title" : "pReddit Homepage!" })
+    return render(request, "Index.html", {})
