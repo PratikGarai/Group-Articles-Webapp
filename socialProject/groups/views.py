32b13a89daca4c2ac3f780e4c7f9c50658ded354
PratikGarai <pratikgarai0208@gmail.com>
2021-01-24 07:36:42 +0530

    join leave functions configured

--- socialProject/groups/views.py
+++ socialProject/groups/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from .models import Group, GroupMember
 
@@ -14,3 +15,31 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+    
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        try :
+            GroupMember.objects.create(user=request.user, group=group)
+        except :
+            # already a member
+            print("Already a member")
+        return super().get(request, *args, **kwargs)
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+    
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        try :
+            GroupMember.objects.filter(user=request.user, group__slug=self.kwargs.get('slug')).get().delete()
+        except :
+            print("Error")
+            # already not a member
+        return super().get(request, *args, **kwargs)
